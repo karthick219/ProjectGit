7e493b74ad400ef1377f6c413767030b31021b11
karthick219 <karthickmesa@gmail.com>
2021-10-08 17:52:16 +0530

    username functionality

--- src/test/java/org/train/Training.java
+++ src/test/java/org/train/Training.java
@@ -1,5 +1,10 @@
 package org.train;
 
 public class Training {
-
+public static void Login() {
+System.out.println("username Functionality");
+}
+public static void main(String[] args) {
+	Login();
+}
 }
